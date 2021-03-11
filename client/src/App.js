62394b9c4498c114665449f6cc98b17f826fb8f5
Yue Su <su2047@gmail.com>
2021-03-11 13:28:09 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,21 @@ import React from "react";
 import { MuiThemeProvider } from "@material-ui/core";
 import { BrowserRouter, Route } from "react-router-dom";
 import { theme } from "./themes/theme";
+import UsersProvider from "./providers/UsersProvider";
 
 import "./App.css";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <MuiThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Route exact path="/" component={null} />
-        <Route path="/dashboard" component={Dashboard} />
-      </BrowserRouter>
-    </MuiThemeProvider>
+    <UsersProvider>
+      <MuiThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Route exact path="/" component={null} />
+          <Route path="/dashboard" component={Dashboard} />
+        </BrowserRouter>
+      </MuiThemeProvider>
+    </UsersProvider>
   );
 }
 
