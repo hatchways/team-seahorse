11bdddd288d5e3333ff72a835fbde6df9010a191
Trevor Shelby <trvrshelby@gmail.com>
2021-03-11 22:03:02 -0600

    Added delete route

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,3 +1,4 @@
+const db = require("../models");
 const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middlewares/authMiddleware");
@@ -23,6 +24,33 @@ router.get("/:productId", async (req, res) => {
     res.status(200).send(JSON.stringify(result));
   } catch {
     res.status(500).send();
+  }
+});
+
+router.delete("/:listId/:productId", async (req, res) => {
+  const transaction = await db.transaction();
+  await db.query(
+    `
+    DELETE FROM "ListProducts"
+    USING "UserLists"
+    WHERE (
+        "ListProducts".list_id = "UserLists".id
+        AND "ListProducts".product_id = ${req.params.productId} AND "ListProducts".list_id = ${req.params.listId}
+        AND "UserLists".user_id = ${req.user.id}
+    )
+  `,
+    { transaction }
+  );
+  const productStillExistsInOtherLists =
+    (await ListProductModel.count({
+      where: { product_id: productId },
+      transaction,
+    })) == 1;
+  if (!productStillExistsInOtherLists) {
+    await ProductModel.destroy({
+      where: { id: productId },
+      transaction,
+    });
   }
 });
 
