d2d02e138a4befe82efc98cf6a80aaf333d80daf
Trevor Shelby <trvrshelby@gmail.com>
2021-03-16 15:15:43 -0500

    Added cover image url.

--- client/src/components/AddNewList.jsx
+++ client/src/components/AddNewList.jsx
@@ -7,11 +7,11 @@ import {
   Dialog,
   DialogTitle,
   Button,
-  TextField
+  TextField,
 } from "@material-ui/core";
-import {Fragment, useState} from "react";
+import { Fragment, useState } from "react";
 import AddIcon from "@material-ui/icons/Add";
-import axios from "axios"
+import axios from "axios";
 
 const useStyles = makeStyles(() => ({
   card: {
@@ -32,60 +32,89 @@ const useStyles = makeStyles(() => ({
   },
   dialogPaper: {
     height: "80vh",
-    width: "80vw"
-  }
+    width: "80vw",
+  },
 }));
 
 const AddNewList = () => {
   const classes = useStyles();
   const [dialogIsOpen, setDialogIsOpen] = useState(false);
   const [title, setTitle] = useState("");
-  const handleOpen = () => setDialogIsOpen(true)
-  const handleClose = () => setDialogIsOpen(false);
+  const [coverImageUrl, setCoverImageUrl] = useState(
+    "http://example.com/image"
+  );
+  const [awaitingResponse, setAwaitingResponse] = useState(false);
+
+  const handleCoverImageUrlChange = (e) => {
+    setCoverImageUrl(e.target.value);
+  };
   const handleTitleChange = (e) => {
-    setTitle(e.target.value)
-  }
+    setTitle(e.target.value);
+  };
   const handleSubmit = async () => {
     try {
-      await axios.create({baseUrl: "localhost:3001", withCredentials: true}).post("/lists", {title})
+      setAwaitingResponse(true);
+      await axios
+        .create({ baseUrl: "localhost:3001", withCredentials: true })
+        .post("/lists", { title, coverImageUrl });
+      setDialogIsOpen(false);
     } catch (error) {
-      console.error(error)
+      setAwaitingResponse(false);
+      console.log(error.response);
     }
-  }
+  };
+
   return (
     <Fragment>
       <Grid item>
         <Card className={classes.card} elevation={0}>
-          <CardActionArea className={classes.action} onClick={handleOpen}>
+          <CardActionArea
+            className={classes.action}
+            onClick={() => setDialogIsOpen(true)}
+          >
             <AddIcon className={classes.icon} color="primary" />
             <Typography>ADD NEW LIST</Typography>
           </CardActionArea>
         </Card>
       </Grid>
       <Dialog
-      classes={{ paper: classes.dialogPaper }}
-      open={dialogIsOpen}
-      onClose={handleClose}
-    >
-      <Grid
-        container
-        alignItems="center"
-        direction="column"
-        spacing={5}
+        classes={{ paper: classes.dialogPaper }}
+        open={dialogIsOpen}
+        onClose={() => setDialogIsOpen(false)}
       >
-        <Grid item>
-          <DialogTitle>Create New List</DialogTitle>
+        <Grid container alignItems="center" direction="column" spacing={5}>
+          <Grid item>
+            <DialogTitle>Create New List</DialogTitle>
+          </Grid>
+          <Grid item>
+            <Typography variant="h6">Add a title</Typography>
+            <TextField
+              label="Title"
+              variant="outlined"
+              onChange={handleTitleChange}
+              autoFocus
+            />
+          </Grid>
+          <Grid item>
+            <Typography variant="h6">Add a Cover Image</Typography>
+            <TextField
+              label="Image URL"
+              variant="outlined"
+              onChange={handleCoverImageUrlChange}
+            />
+          </Grid>
+          <Grid item>
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={handleSubmit}
+              disabled={awaitingResponse}
+            >
+              Create List
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item>
-          <TextField label="Title" variant="outlined" id="list-title" onChange={handleTitleChange} />
-        </Grid>
-        <Grid item>
-          <Button variant="contained" color="secondary" onClick={handleSubmit}>
-            Create List
-          </Button>
-        </Grid>
-      </Grid>
-    </Dialog>
+      </Dialog>
     </Fragment>
   );
 };
