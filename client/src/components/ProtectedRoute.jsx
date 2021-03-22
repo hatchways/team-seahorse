81304bfac1ec92513e774d99c11ca614773a1136
Trevor Shelby <trvrshelby@gmail.com>
2021-03-22 09:35:27 -0500

    changed route references to be hyphen-spaced and not camelcase.

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -2,19 +2,15 @@ import React, { useContext } from "react";
 import { Route, Redirect } from "react-router-dom";
 import { userContext as context } from "../providers/UsersProvider";
 
-const ProtectedRoute = ({component : Component, ...rest}) => {
+const ProtectedRoute = ({ component: Component, ...rest }) => {
   const userContext = useContext(context);
   const { user, token } = userContext;
 
   return (
     <Route
       {...rest}
-      render={props =>
-        !user && !token ? (
-          <Redirect to='/signin' />
-        ) : (
-          <Component {...props} />
-        )
+      render={(props) =>
+        !user && !token ? <Redirect to="/sign-in" /> : <Component {...props} />
       }
     />
   );
