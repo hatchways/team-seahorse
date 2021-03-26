464a0149df8b92e3e53d04a0e772dad6ea01de04
Rolando Morales <rolando.moralesjr21@gmail.com>
2021-03-26 03:41:24 -1000

    Working code for ebay product links. No node-cron

--- server/scraper-service/index.js
+++ server/scraper-service/index.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const cron = require("node-cron");
 const Queue = require("bull");
-const { scrapeAmazon, scrapeEbay } = require("../services");
-const http = require("http");
+const { scrapeAmazon, scrapeEbay, amazonScraper } = require("../services");
 const axios = require("axios");
 
 var seconds = new Date().getTime() / 1000;
@@ -10,82 +9,91 @@ const app = express();
 
 const ebayQueue = new Queue("ebay");
 const ebayQueue2 = new Queue("ebay2");
+
 const amazonQueue = new Queue("amazon");
+const amazonQueue2 = new Queue("amazon2");
+
 const craigslistQueue = new Queue("craigslist");
+const craigslistQueue2 = new Queue("craigslist2");
 
 const options = {
-  // delay: 60000, // 1 min in ms
-  attempts: 1,
+  attempts: 2,
 };
 
 ebayQueue.process(async (job) => {
-  const { link, id, currentPrice, previousPrice } = job.data;
-  console.log(`1 : Job Starting: ${id}`);
+  const { link, id, currentPrice, name } = job.data;
+  console.log(`Ebay 1 : Job Starting: ${id}`);
 
   try {
-    const results = await scrapeEbay(link);
-    if (results === Error) {
-      console.log(`1 : JOB FAILED: ${id}`);
-      throw new Error();
-    }
-
+    const results = await scrapeEbayProduct(link, id, 1);
     const { data } = await checkPriceChange(
       currentPrice,
-      previousPrice,
       results.price,
-      id
+      id,
+      name
     );
 
     console.log(data);
-
     console.log(new Date().getTime() / 1000 - seconds);
+
+    await job.moveToCompleted();
   } catch (error) {
-    console.error(error);
+    console.error(`Product : ${id} Failed`);
     throw new Error(error);
   }
 });
 
 ebayQueue2.process(async (job) => {
-  const { link, id, currentPrice, previousPrice } = job.data;
-  console.log(`2 : Job Starting: ${id}`);
+  const { link, id, currentPrice, name } = job.data;
+  console.log(`Ebay 2 : Job Starting: ${id}`);
 
   try {
-    const results = await scrapeEbay(link);
-    if (results === Error) {
-      console.log(`2 : JOB FAILED: ${id}`);
-      throw new Error();
-    }
-
-    console.log(results);
-
-    //check for price change
+    const results = await scrapeEbayProduct(link, id, 2);
     const { data } = await checkPriceChange(
       currentPrice,
-      previousPrice,
       results.price,
-      id
+      id,
+      name
     );
+
     console.log(data);
     console.log(new Date().getTime() / 1000 - seconds);
+
+    await job.moveToCompleted();
   } catch (error) {
-    console.error(error);
+    console.error(`Product : ${id} Failed`);
     throw new Error(error);
   }
 });
 
-const checkPriceChange = (
-  currentPrice,
-  fetchedPrice,
-  productId
-) => {
+amazonQueue2.process(async (job) => {
+  const { link, id, currentPrice, name } = job.data;
+
+  console.log(`Amazon 2 : Job Starting: ${id}`);
+
+  try {
+    const results = await scrapeAmazonProduct(link, id, 2);
+    console.log(results);
+
+    await job.moveToCompleted();
+  } catch (error) {
+    console.error(`Product : ${id} Failed`);
+    throw new Error(error);
+  }
+});
+
+const checkPriceChange = (currentPrice, fetchedPrice, productId, name) => {
+  console.log(name);
+
   if (currentPrice !== fetchedPrice) {
     const body = {
       currentPrice,
-      id: productId,
       newPrice: fetchedPrice,
+      id: productId,
+      name,
     };
 
-    return axios.put("http://localhost:3001/products/update", body);
+    return axios.post("http://localhost:3001/notification/price", body);
   }
 
   return { data: { msg: "No Price Change" } };
@@ -95,23 +103,75 @@ const wrapper = async () => {
   const { data: allProducts } = await axios.get(
     "http://localhost:3001/products/getAll"
   );
-  let alt = true;
+
+  let switcherEbay = true;
+  let switcherAmazon = true;
+  let switcherCraigslist = true;
+
   console.log("WRAPPER FUNCTION EXECUTED ------------------------");
-  //add check if which scraper function
   console.log(await ebayQueue.count());
-  if (true) {
-    allProducts.forEach(async (product) => {
-      if (alt) {
-        ebayQueue.add(product, options);
-      } else {
-        ebayQueue2.add(product, options);
-      }
-      alt = !alt;
-    });
+
+  allProducts.forEach(async (product) => {
+    product.currentPrice = parseFloat(product.currentPrice);
+
+    if (product.link.includes("ebay")) {
+      console.log("ebay prod");
+      distributeJob(ebayQueue, ebayQueue2, switcherEbay);
+
+      switcherEbay = !switcherEbay;
+    } else if (product.link.includes("amazon")) {
+      console.log("amazon prod");
+      distributeJob(amazonQueue, amazonQueue2, switcherAmazon);
+
+      switcherAmazon = !switcherAmazon;
+    } else if (product.link.includes("craigslist")) {
+      console.log("craigslist prod");
+      distributeJob(craigslistQueue, craigslistQueue2, switcherCraigslist);
+
+      switcherCraigslist = !switcherCraigslist;
+    }
+  });
+};
+
+const distributeJob = (queue1, queue2, bool) => {
+  if (bool) {
+    queue1.add(product, options);
+  } else {
+    queue2.add(product, options);
   }
 };
 
-wrapper();
+const scrapeEbayProduct = async (link, id, queueNumber) => {
+  const results = await scrapeEbay(link);
+  if (results === Error) {
+    console.log(`${queueNumber} : JOB FAILED: ${id}`);
+    throw new Error();
+  }
+
+  return results;
+};
+
+const scrapeAmazonProduct = async (link, id, queueNumber) => {
+  const results = await scrapeAmazon(link);
+  if (results === Error) {
+    console.log(`${queueNumber} : JOB FAILED: ${id}`);
+    throw new Error();
+  }
+
+  return results;
+};
+
+// wrapper();
+
+const wrapper2 = async () => {
+  const results = await amazonScraper(
+    "https://www.amazon.com/Lenovo-Computer-Touchscreen-Display-Quad-Core-i5-1035G1/dp/B08HGQP26V/ref=sr_1_1_sspa?dchild=1&keywords=laptop&qid=1616763457&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzTk1WR1lDUVNKV1JBJmVuY3J5cHRlZElkPUEwNzg4OTYyMU0yVUFSRFlIMlEwWiZlbmNyeXB0ZWRBZElkPUEwNTQ4MjI3MUkwSEdCUTdCWVFFNSZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU="
+  );
+
+  console.log(results);
+};
+
+wrapper2();
 
 app.listen(8000, () => {
   console.log("Running on port 8k");
