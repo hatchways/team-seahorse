f0cce12dc498c459842489517118f5d42ebf0896
Trevor Shelby <trvrshelby@gmail.com>
2021-03-30 10:19:54 -0500

    some small fixes.

--- client/src/components/AddProductDialog.jsx
+++ client/src/components/AddProductDialog.jsx
@@ -1,8 +1,6 @@
 import {
   Button,
-  Card,
   Dialog,
-  DialogContent,
   Grid,
   IconButton,
   makeStyles,
@@ -11,13 +9,12 @@ import {
   TextField,
   Typography,
 } from "@material-ui/core";
-import React, { useContext, useEffect, useState } from "react";
-import PropTypes from "prop-types";
-import CloseIcon from "@material-ui/icons/Close";
-import sample from "../images/productIcon.jpg";
+import React, { useContext, useEffect, useState, forwardRef } from "react";
+import ProductConfirmation from "./ProductConfirmation";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import { userContext as context } from "../providers/UsersProvider";
 import EditListDialog from "./EditListDialog";
+import axios from "axios";
 
 const useStyles = makeStyles(() => ({
   paper: {
@@ -69,23 +66,22 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
-const submitHandler = () => {
-  //Use a route for adding a product to list
-};
-
 const AddProductDialog = () => {
   const classes = useStyles();
-  const userContext = useContext(context);
-
-  const {
-    updateIsAddingProd,
-    isAddingProd,
-  } = userContext;
+  const [productUrl, setProductUrl] = useState("");
+  const [isConfirming, setIsConfirming] = useState(false);
+  const { updateIsAddingProd, isAddingProd, currentList } = useContext(context);
 
   useEffect(() => {
     //Get the products of the list then setLoading(false)
   }, []);
 
+  const submitHandler = async () => {
+    await axios.create({ withCredentials: true }).post("/products/", {
+      url: productUrl,
+      listId: currentList.id,
+    });
+  };
   return (
     <>
       <EditListDialog />
@@ -125,40 +121,52 @@ const AddProductDialog = () => {
                   InputProps={{
                     disableUnderline: true,
                   }}
+                  onChange={(e) => setProductUrl(e.target.value)}
                 ></TextField>
               </Grid>
+              {!currentList && (
+                <>
+                  <Grid item>
+                    <Typography
+                      variant="h5"
+                      className={classes.selectListHeader}
+                    >
+                      Select List
+                    </Typography>
+                  </Grid>
 
-              <Grid item>
-                <Typography variant="h5" className={classes.selectListHeader}>
-                  Select List
-                </Typography>
-              </Grid>
-
-              <Grid item className={classes.grid}>
-                <TextField
-                  select
-                  InputProps={{
-                    disableUnderline: true,
-                  }}
-                  label="Select List"
-                  style={{
-                    width: "250px",
-                    backgroundColor: "white",
-                    borderRadius: "7px",
-                  }}
-                ></TextField>
-              </Grid>
+                  <Grid item className={classes.grid}>
+                    <TextField
+                      select
+                      InputProps={{
+                        disableUnderline: true,
+                      }}
+                      label="Select List"
+                      style={{
+                        width: "250px",
+                        backgroundColor: "white",
+                        borderRadius: "7px",
+                      }}
+                    ></TextField>
+                  </Grid>
+                </>
+              )}
 
               <Button
                 className={classes.button}
                 color="primary"
                 variant="contained"
-                onClick={() => submitHandler()}
+                onClick={() => setIsConfirming(true)}
               >
                 Add Item
               </Button>
             </Grid>
           </Paper>
+        </Slide>
+        <Slide direction="left" in={isConfirming} mountOnEnter>
+          <div>
+            <ProductConfirmation setModal={setIsConfirming} />
+          </div>
         </Slide>
       </Dialog>
     </>
