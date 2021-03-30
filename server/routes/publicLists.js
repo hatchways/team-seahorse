38de72c14b7511d922d60b8070953335b4d7c96f
Yue Su <su2047@gmail.com>
2021-03-30 20:21:46 -0300

    added getProductsOfList

--- server/routes/publicLists.js
+++ server/routes/publicLists.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const UserListModel = require("../models/userListModel");
+const db = require("../models");
 
 const getLists = async (req, res) => {
   try {
@@ -15,6 +16,29 @@ const getLists = async (req, res) => {
   }
 };
 
+const getProductsofList = async (req, res) => {
+  const listId = req.params.listId;
+  try {
+    const listFound = await UserListModel.findOne({
+      where: {
+        id: listId,
+        isPrivate: false,
+      },
+    });
+
+    if (listFound) {
+      const [products] = await db.query(`
+        SELECT "Products".name, "Products".link, "Products".id, "Products"."currentPrice", "Products"."previousPrice" FROM "ListProducts" 
+        JOIN "Products" ON ("ListProducts"."productId" = "Products".id)
+        JOIN "UserLists" ON ("ListProducts"."listId" = "UserLists".id)
+        WHERE ( "ListProducts"."listId" = ${parseInt(listId)} )
+    `);
+      res.status(200).send(products);
+    }
+  } catch (error) {}
+};
+
 router.get("/", getLists);
+router.get("/:listId", getProductsofList);
 
 module.exports = router;
